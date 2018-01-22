1b8ddfa75ef5872c5e1f4403de83ec2fd0133a6c
Jo Schreurs <jootjecadeautje@gmail.com>
2018-01-22 09:36:17 +0000

    Inloggen en register werkt

--- website/application.py
+++ website/application.py
@@ -28,9 +28,9 @@ Session(app)
 db = SQL("sqlite:///website.db")
 
 
-@app.route("/")
+@app.route("/homepage")
 @login_required
-def index():
+def homepage():
 
     return apology("TODO")
 
@@ -44,14 +44,6 @@ def login():
     # if user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username")
-
-        # ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password")
-
         # query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
 
@@ -63,7 +55,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # redirect user to home page
-        return redirect(url_for("index"))
+        return redirect(url_for("homepage"))
 
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
@@ -78,7 +70,6 @@ def logout():
 
     # redirect user to login form
     return redirect(url_for("login"))
-
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -107,7 +98,7 @@ def register():
         session["user_id"] = rows[0]["id"]
 
         # stuur de gebruiker naar de homepagina
-        return redirect(url_for("homepage.html"))
+        return redirect(url_for("homepage"))
 
     else:
         return render_template("register.html")
