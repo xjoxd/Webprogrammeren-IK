b6e73c74ba6faa3c36450cc34032d223b460bccd
xjoxd <31687623+xjoxd@users.noreply.github.com>
2018-01-23 15:40:41 +0100

    Create homepage.py

---
+++ website/homepage.py
@@ -0,0 +1,17 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from functools import wraps
+from flask_session import Session
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///website.db")
+
+def display():
+    pictures = db.execute("SELECT * FROM images")
+
