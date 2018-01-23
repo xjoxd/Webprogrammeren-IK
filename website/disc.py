36bba22bb74f35b278545ec731bd0d68dd3a0172
Quintin Raemaekers <quintin.raemaekers@student.uva.nl>
2018-01-23 14:22:15 +0000

    discover

---
+++ website/disc.py
@@ -0,0 +1,39 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+
+from helpers import *
+from log import *
+from reg import *
+from upload import *
+
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///website.db")
+
+
+def disc():
+
+    # als de gebruiker via POST kwam
+    if request.method == "POST":
+
+        # ervoor zorgen dat de wachtwoorden hetzelfde zijn
+        tag = request.form.get("#tag")
+
+        randoms = db.execute(" SELECT * FROM users WHERE tag =: ", tag=tag)
+        print(randoms)
+
+        #while randoms:
+        #   profile():
+        #
+        #   if like:
+        #       follow
+        #
+        #   profile = old
+        #
+        #   rerun
+
+#def profile(randoms):
