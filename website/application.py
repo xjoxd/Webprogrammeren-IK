a48d436113fba1fe422422969f05b20b3b683ea4
Quintin Raemaekers <quintin.raemaekers@student.uva.nl>
2018-01-30 23:00:02 +0000

    .

--- website/application.py
+++ website/application.py
@@ -52,13 +52,11 @@ def homepage():
             session["image_id"] = request.form.get("comment")
             return redirect(url_for("comment"))
         else:
-            get_comment = get_comments()
             pictures = display()
-            return render_template("homepage.html", images=pictures, comments=get_comment)
+            return render_template("homepage.html", images=pictures)
     else:
-        get_comment = get_comments()
         pictures = display()
-        return render_template("homepage.html", images=pictures, comments=get_comment)
+        return render_template("homepage.html", images=pictures)
 
 @app.route("/like", methods=["POST"])
 def like():
