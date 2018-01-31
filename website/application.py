639a42f1d43f238fa981ef69fb0613413b5d28e2
Mark Muller <mark.muller99@gmail.com>
2018-01-31 14:06:32 +0000

    giffs

--- website/application.py
+++ website/application.py
@@ -261,8 +261,8 @@ def storegif():
         filename = request.args.get('url')
         model.giphy(filename)
         return redirect(url_for("post"))
- 
+
 @app.route("/getgif/<gifje>", methods=["GET"])
 def getgif(gifje):
-return redirect("https://media1.giphy.com/media/" + gifje+"/giphy.gif")
+    return redirect("https://media1.giphy.com/media/" + gifje+"/giphy.gif")
 
