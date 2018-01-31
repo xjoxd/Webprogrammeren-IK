1d67e7195242545712eed6e40ebbbba28b3010c7
xjoxd <31687623+xjoxd@users.noreply.github.com>
2018-01-31 22:54:11 +0100

    Update application.py

--- website/application.py
+++ website/application.py
@@ -33,7 +33,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# configure CS50 Library to use SQLite database
+# CS50 Library configureren om SQLite database te gebruiken.
 db = SQL("sqlite:///website.db")
 
 
